75d9d472e905f34071f4f8efeb24261f9c8e7bee
Josh Goldman <josheligoldman@gmail.com>
2026-01-31 16:18:15 -0500

    added quick to call cli.sh for convenience

---
+++ cli.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+DATA_DIR="data/zeolite_topos"
+OUTPUT_DIR="data/zeolite_gavrog"
+ERROR_DIR="data/zeolite_errors"
+THREADS=24
+TIMEOUT=30000
+
+LOG_FILE="data/zeolites.log"
+# Make the parent of the log file
+mkdir -p $(dirname $LOG_FILE)
+
+node src/cli/cli.js -i $DATA_DIR -o $OUTPUT_DIR -e $ERROR_DIR -t $THREADS -ms $TIMEOUT > $LOG_FILE
